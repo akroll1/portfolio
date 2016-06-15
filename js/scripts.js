3c035aa2907ea04154186909a9cbc6bfb3d0c001
Andrew Kroll <andrew.kroll@yahoo.com>
2016-06-15 07:07:43 -0400

    added american void

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,13 @@ $(document).ready(function(){
 
 
 	var html1 = '<div class="switch"><a href="https://damp-woodland-2282.herokuapp.com"><h3>E-Commerce Coffee Site</h3><p>A fully functional e-commerce site powered by Node.js and utilizing Stripes payment system.</p></a></div>';
-	var html2 = '<div class="switch"><a href="http://goblingame.andrewkroll.com"><h3>Goblin Game</h3><p>A simple game utilizing canvas. Use the arrow keys to move our hero around the board!</p></a></div>';
+	// var html2 = '<div class="switch"><a href="http://goblingame.andrewkroll.com"><h3>Goblin Game</h3><p>A simple game utilizing canvas. Use the arrow keys to move our hero around the board!</p></a></div>';
+	var html2 = '<div class="switch"><a href="https://obscure-hollows-35112.herokuapp.com"><h3>IoTAndrew.com</h3><p>My blog on IoT, the internet of things. I touch on recent developments in IoT as well as IoT and artificial intelligence implications.</p></a></div>';
 	var html3 = '<div class="switch"><a href="https://damp-woodland-2282.herokuapp.com"><h3>OMDB Movie Search</h3><p>A searchable movie database powered by omdb.coms API.</p></a></div>';
 	var html4 = '<div class="switch"><a href="https://thawing-basin-32203.herokuapp.com"><h3>Is this site on?</h3><p>Is this site on? Did you leave this site on?</p></a></div>';
 	var html5 = '<div class="switch"><a href="http://slots.andrewkroll.com"><h3>Slot Machine</h3><p>A slot machine game utilizing jQuery that I developed on my own, from design to logic.</p></a></div>';
 	var html6 = '<div class="switch"><a href="http://youtube.andrewkroll.com"><h3>OMDB Movie Search</h3><p>A YouTube clone I developed with real search from the YouTube API - check it out!</p></a></div>';
-	var html7 = '<div class="switch"><a href="https://obscure-hollows-35112.herokuapp.com"><h3>IoTAndrew</h3><p>My blog on IoT, the internet of things. I touch on recent developments in IoT as well as IoT and artificial intelligence implications.</p></a></div>';
+	var html7 = '<div class="switch"><a href="https://salty-savannah-41915.herokuapp.com/"><h3>AmericanVoid.com</h3><p>My blog on the American political scene, speaking to those planted firmly in the middle.</p></a></div>';
 	var html8 = '<div class="switch"><a href=""><h3>Lego(IoT) House</h3><p>My Lego(IoT) House. No link here, this is a real, physical thing! See the description above in the Featured Project section.</p></a></div>';
 	var html9 = '<div class="switch"><a href=""><h3>IoT Medicine Bottle Reminder Project</h3><p>A secret IoT project I am still working on, currently under development.</p></a></div>';
 
@@ -24,14 +25,14 @@ $(document).ready(function(){
 	$('.pane1').mouseenter(function(){
 		$('.pane1 h4').hide();
 		$('.pane1 img').hide();
-
+		// $('.pane1 img').css('opacity','0.1').fadeIn(200);
      	$(html1).fadeIn(500).appendTo($('.pane1'));
 	});
 
 	$('.pane1').mouseleave(function(){
 		$('.pane1 h4').show();
 		$('.pane1 img').show();
-		
+		// $('.pane1 img').css('opacity','1.0').fadeIn(1500);
 		$('.switch').remove();
 	});
 //================= PANE 2 =================
